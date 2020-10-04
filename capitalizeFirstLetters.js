c2d97e63e4ca034293d0eaf5430e0d05955852f9
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-10-05 00:02:53 +0200

    make a test folder and organize tests

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,5 +1,3 @@
-const assert = require("assert");
-
 //the capitalizeFirstLetters function
 function capitalizeFirstLetters(input) {
   let output = "";
@@ -13,16 +11,4 @@ function capitalizeFirstLetters(input) {
   return output;
 }
 
-//check that capitalizeFirstLetters is a function
-assert.strictEqual(typeof capitalizeFirstLetters, "function");
-//check that capitalizeFirstLetters accept 1 argument
-assert.strictEqual(capitalizeFirstLetters.length, 1);
-//check that capitalizeFirstLetters transform i am learning TDD correctly
-assert.strictEqual(
-  capitalizeFirstLetters("i am learning TDD"),
-  "I Am Learning TDD"
-);
-//check that capitalizeFirstLetters transfrom j correctly
-assert.strictEqual(capitalizeFirstLetters("j"), "J");
-//check that capitalizeFirstLetters works with empty string
-assert.strictEqual(capitalizeFirstLetters(""), "");
+module.exports = capitalizeFirstLetters;
