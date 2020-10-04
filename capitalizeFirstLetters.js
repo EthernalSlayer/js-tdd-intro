9a3a1f54a56fdafd6d698067111afada7c212a11
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-10-04 22:42:09 +0200

    tests validation

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,6 +1,17 @@
 const assert = require("assert");
 
 //the capitalizeFirstLetters function
+function capitalizeFirstLetters(input) {
+  let output = "";
+  for (let i = 0; i < input.length; i++) {
+    if (i === 0 || input[i - 1] === " ") {
+      output += input[i].toUpperCase();
+    } else {
+      output += input[i];
+    }
+  }
+  return output;
+}
 
 //check that capitalizeFirstLetters is a function
 assert.strictEqual(typeof capitalizeFirstLetters, "function");
